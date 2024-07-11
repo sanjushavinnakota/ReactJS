d99efa45858e787c2018659a504cc47d79bb7d68
SAI PRUDHVI <saiprudhvi@SAIs-MacBook-Air.local>
2024-07-11 21:05:54 +0530

    changes updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Frame1 from './components/Frame1/Frame1';
+import Frame2 from './components/Frame2/Frame2';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    
+    <><Navbar />
+    <Frame1 />
+    < Frame2   />
+    <Footer />
+
+    
+    
+    </>
+
+  );
+}
+
+export default App;
